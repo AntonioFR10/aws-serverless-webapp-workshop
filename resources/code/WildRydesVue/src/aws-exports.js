f1627f997de0d8ee0b0c776ef7c6ca0f4fe99175
AntonioFR10 <78943551+AntonioFR10@users.noreply.github.com>
2026-05-01 12:32:19 -0600

    Add aws-exports.js for AWS configuration

---
+++ resources/code/WildRydesVue/src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_kZeCueo7h',
+  aws_user_pools_web_client_id: '1tgpulr9f61a9s0dqhpnphva3d'
+};
+
+export default awsmobile;
