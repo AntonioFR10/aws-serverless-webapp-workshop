36e1bb852d889003f908b17988dfcfee67460531
AntonioFR10 <78943551+AntonioFR10@users.noreply.github.com>
2026-05-01 13:35:32 -0600

    Update API invokeUrl in config.js

--- resources/code/WildRydesVue/public/js/config.js
+++ resources/code/WildRydesVue/public/js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1'
     },
     api: {
-        invokeUrl: ''
+        invokeUrl: 'https://cmmok3et2j.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
