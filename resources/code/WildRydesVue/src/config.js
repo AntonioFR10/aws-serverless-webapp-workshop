4608c22aafb4301d2caa59db0ff40b915056af0d
AntonioFR10 <78943551+AntonioFR10@users.noreply.github.com>
2026-05-01 14:13:25 -0600

    Update API invoke URL in config.js

--- resources/code/WildRydesVue/src/config.js
+++ resources/code/WildRydesVue/src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://cmmok3et2j.execute-api.us-east-1.amazonaws.com/prod' 
     }
 }
